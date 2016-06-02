9b4ce864e3fa2fa0993b9bfe29ac5bdbe8e987bf
k-zon_000 <k-zon_000@KayraNote>
2016-06-02 22:25:56 +0300

    #

--- src/com/exponentus/localization/LanguageCode.java
+++ src/com/exponentus/localization/LanguageCode.java
@@ -5,10 +5,8 @@ package com.exponentus.localization;
  *
  */
 public enum LanguageCode {
-	UNKNOWN(0, "english"), ENG(45, "english"), RUS(570, "russian"), KAZ(255, "english"), BUL(115, "english"),
-	POR(545, "portuguese"), SPA(230, "spanish"), CHI(315, "english"), DEU(316, "german"), @Deprecated
-	CHN(3150, "english"), @Deprecated
-	CHO(3151, "english");
+	UNKNOWN(0, "unknown"), ENG(45, "english"), RUS(570, "russian"), KAZ(255, "kazakh"), BUL(115, "bulgarian"), POR(545, "portuguese"), SPA(230,
+	        "spanish"), CHI(315, "chinese"), DEU(316, "german"), @Deprecated CHN(3150, "chinese"), @Deprecated CHO(3151, "chinese");
 
 	private int code;
 	private String lang;
@@ -21,7 +19,8 @@ public enum LanguageCode {
 	public int getCode() {
 		return code;
 	}
-	public String getLang(){
+
+	public String getLang() {
 		return lang;
 	}
 

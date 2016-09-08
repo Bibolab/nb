439dfc9ebc454e3e7c9f9926dbfbcfe152dfd278
John <john_198708@mail.ru>
2016-09-09 01:05:10 +0600

    commit

--- rule/Administrator/Resources/scripts/administrator/page/view/LogView.java
+++ rule/Administrator/Resources/scripts/administrator/page/view/LogView.java
@@ -5,12 +5,17 @@ import com.exponentus.dataengine.jpa.ViewPage;
 import com.exponentus.log.LogFiles;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
+import com.exponentus.scripting.actions._Action;
+import com.exponentus.scripting.actions._ActionBar;
+import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.scripting.event._DoPage;
 
 public class LogView extends _DoPage {
 
 	@Override
 	public void doGET(_Session session, _WebFormData formData) {
+        _ActionBar actionBar = new _ActionBar(session);
+        actionBar.addAction(new _Action("Delete", "Delete log", _ActionType.DELETE_DOCUMENT));
 		String dir = formData.getValueSilently("category");
 		int pageNum = formData.getNumberValueSilently("page", 1);
 		int pageSize = session.pageSize;
@@ -22,6 +27,7 @@ public class LogView extends _DoPage {
 			pageNum = maxPage;
 		}
 		ViewPage vp = new ViewPage(logs.getLogFiles(), count, maxPage, pageNum);
+        addContent(actionBar);
 		addContent(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum());
 	}
 }

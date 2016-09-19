6fc88aab62e43966b2d1e3b40fab490535967353
John <john_198708@mail.ru>
2016-09-19 22:52:10 +0600

    commit

--- rule/Administrator/Resources/scripts/administrator/page/view/LanguageView.java
+++ rule/Administrator/Resources/scripts/administrator/page/view/LanguageView.java
@@ -6,6 +6,9 @@ import com.exponentus.scripting.IPOJOObject;
 import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
+import com.exponentus.scripting.actions._Action;
+import com.exponentus.scripting.actions._ActionBar;
+import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.scripting.event._DoPage;
 
 import administrator.dao.LanguageDAO;
@@ -18,6 +21,12 @@ public class LanguageView extends _DoPage {
 		LanguageDAO dao = new LanguageDAO(session);
 		String keyword = formData.getValueSilently("keyword");
 		List<? extends IPOJOObject> list = dao.findAll();
+		_ActionBar actionBar = new _ActionBar(session);
+		_Action newDocAction = new _Action("New language", "Registration of the new language", "new_lang");
+		newDocAction.setURL("Provider?id=language-form");
+		actionBar.addAction(newDocAction);
+		actionBar.addAction(new _Action("Delete", "Delete language", _ActionType.DELETE_DOCUMENT));
+		addContent(actionBar);
 		addContent(new _POJOListWrapper(list, 0, dao.getCount(), 0, session, keyword));
 	}
 }
